c4aa68ca29b75ffcc7ab5fda7ed05ed64713e84e
BakuEmilov <emilovbaku530@gmail.com>
2023-11-06 18:33:32 +0600

    first task completed

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-let a = 1;
-let b = 0;
+let x = 1;
 
-if (a > 0) {
-  b = a;
-} else {
-  b += 1;
+switch (x) {
+  case 2:
+    console.log("не та цифра");
+    break;
+  case 5:
+    console.log("не та цифра");
+    break;
+  case 1:
+    console.log("точно в цель");
+    break;
+  case 7:
+    console.log("не так цифра");
+    break;
+
+  default:
+    console.log("облажался брат");
 }
-
-console.log(b);
